f38566b391810b2a739ba6221bd49985d794db21
Leon Kiessle <leon.kiessle@student.kit.edu>
2026-09-02 20:14:23 +0200

    changed code to find library by absolute path

--- polymetis/python/torchcontrol/transform/rotation.py
+++ polymetis/python/torchcontrol/transform/rotation.py
@@ -8,14 +8,26 @@ from ctypes.util import find_library
 
 import torch
 
+def load_torch_lib(name: str):
+
+    import os
+
+    path = find_library(name)
+    if path is None:
+        raise ImportError(
+            f"Could not find 'lib{name}.so' library. "
+            "Make sure it is built and available in the library path."
+        )
+
+    # Pull library from conda env, as env must exist for novometis
+    path_prefix = os.environ['CONDA_PREFIX']
+    full_path = os.path.join(path_prefix, "lib", path)
+
+    torch.classes.load_library(full_path)
+
+
 # load the custom C++ library for rotation operations
-torchrot = find_library("torchrot")
-if torchrot is None:
-    raise ImportError(
-        "Could not find 'libtorchrot.so' library. "
-        "Make sure it is built and available in the library path."
-    )
-torch.classes.load_library(torchrot)
+load_torch_lib("torchrot")
 
 functional = torch.ops.torchrot
 
